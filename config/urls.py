0afebb29974fddcbf8d202857778a123c68ab55b
Scott <saseaa37@gmail.com>
2023-04-02 10:47:21 -0600

    4-2-23-2

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
+    path("issues/", include("issues.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
